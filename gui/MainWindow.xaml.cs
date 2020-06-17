3664a42e566fed6276e264aa76ebe1933f20f440
unknown <rozanski.michal.94@gmail.com>
2020-06-17 11:08:21 +0200

    first version of GUI

---
+++ gui/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<SummarizerControl> summarizers = new List<SummarizerControl>();
+        private List<List<string>> ParseXml()
+        {
+            var modifiers = new List<string>   {"", "NOT", "Absolutely" };
+            var quantifiers = new List<string> {"", "Over 100", "half" };
+            var qualifiers = new List<string>  {"", "salary over 1000", "small salary" };
+            var summarizers = new List<string> {"", "young", "old" };
+            var connectors = new List<string>  {"", "AND", "OR" };
+
+            return new List<List<string>> { modifiers, quantifiers, qualifiers, summarizers, connectors };
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var a = ParseXml();
+
+            var s1 = new SummarizerControl(1, a[0], a[1], a[2], a[3], a[4]);
+            SPMain.Children.Add(s1);
+            var s2 = new SummarizerControl(2, a[0], a[1], a[2], a[3], a[4]);
+            SPMain.Children.Add(s2);
+
+            summarizers.Add(s1);
+            summarizers.Add(s2);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var a = summarizers[0].GetQuantifierParameters();
+            var b = summarizers[0].GetId();
+            var c = summarizers[0].GetQualifierParameters();
+            var d = summarizers[0].GetSummarizerParameters();
+
+            var z = summarizers[1].GetQuantifierParameters();
+            var f = summarizers[1].GetId();
+            var g = summarizers[1].GetQualifierParameters();
+            var h = summarizers[1].GetSummarizerParameters();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ gui/SummarizerControl.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gui
+{
+
+    public class SummaryParameters
+    {
+        public List<string> modifiers = new List<string>();
+        public string value;
+        public string connector;
+    }
+
+    /// <summary>
+    /// Interaction logic for SummarizerControl.xaml
+    /// </summary>
+    public partial class SummarizerControl : UserControl
+    {
+        private int id; 
+        public SummarizerControl(int id, List<string> modifiers, List<string> quantifiers, List<string> qualifiers, List<string> summarizers, List<string> connectors)
+        {
+            this.id = id;
+            InitializeComponent();
+
+            LName.Content = "Summarizer " + id.ToString();
+
+            // Modifiers
+            CBQuantifier1.ItemsSource = modifiers;
+            CBQuantifier2.ItemsSource = modifiers;
+
+            CBQualifier1.ItemsSource = modifiers;
+            CBQualifier2.ItemsSource = modifiers;
+            CBQualifier5.ItemsSource = modifiers;
+            CBQualifier6.ItemsSource = modifiers;
+
+            CBSummarizer1.ItemsSource = modifiers;
+            CBSummarizer2.ItemsSource = modifiers;
+            CBSummarizer5.ItemsSource = modifiers;
+            CBSummarizer6.ItemsSource = modifiers;
+
+            // Quantifiers
+            CBQuantifier3.ItemsSource = quantifiers;
+
+            // Qualifiers
+            CBQualifier3.ItemsSource = qualifiers;
+            CBQualifier7.ItemsSource = qualifiers;
+
+            // Summarizers
+            CBSummarizer3.ItemsSource = summarizers;
+            CBSummarizer7.ItemsSource = summarizers;
+
+            // Connectors
+            CBSummarizer4.ItemsSource = connectors;
+            CBQualifier4.ItemsSource = connectors;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public SummaryParameters GetQuantifierParameters()
+        {
+            var a = new SummaryParameters()
+            {
+                modifiers = new List<string> { CBQuantifier1.Text, CBQuantifier2.Text },
+                value = CBQuantifier3.Text,
+            };
+            return a;
+        }
+
+        public List<SummaryParameters> GetQualifierParameters()
+        {
+            var a1 = new SummaryParameters()
+            {
+                modifiers = new List<string> { CBQualifier1.Text, CBQualifier2.Text },
+                value = CBQualifier3.Text,
+                connector = "",
+            };
+
+            var a2 = new SummaryParameters()
+            {
+                modifiers = new List<string> { CBQualifier5.Text, CBQualifier6.Text },
+                value = CBQualifier7.Text,
+                connector = CBQualifier4.Text,
+            };
+            return new List<SummaryParameters> { a1, a2 };
+        }
+
+        public List<SummaryParameters> GetSummarizerParameters()
+        {
+            var a1 = new SummaryParameters()
+            {
+                modifiers = new List<string> { CBSummarizer1.Text, CBSummarizer2.Text },
+                value = CBSummarizer3.Text,
+                connector = "",
+            };
+
+            var a2 = new SummaryParameters()
+            {
+                modifiers = new List<string> { CBSummarizer5.Text, CBSummarizer6.Text },
+                value = CBSummarizer7.Text,
+                connector = CBSummarizer4.Text,
+            };
+
+            return new List<SummaryParameters> { a1, a2 };
+        }
+
+    }
+}
